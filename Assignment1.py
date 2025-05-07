f7987c5b91ffeb7b01be9bbe90c66ad936640282
rll3811 <155147202+rll3811@users.noreply.github.com>
2025-05-07 23:01:59 +0530

    Create Assignment1.py

---
+++ Assignment1.py
@@ -0,0 +1,39 @@
+''''#### Assignment: 01.
+## Task: 01''''
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c=print("The sum of", a, "and", b, "is:", a+b)
+d=print("The subtraction of", a, "and", b, "is:", a-b)
+e=print("The multiplication of", a, "and", b, "is:", a*b)
+f=print("The division of", a, "and", b, "is:", a/b)
+
+
+'''## Task: 02'''
+
+FstName = input("Enter your first name:")
+LstName = input("Enter your last name:")
+print("Hello,", FstName + " " + LstName+"! Welcome to the python program.")
+
+
+
+#########################################################################
+#############
+#########################################################################
+
+''''#### Assignment: 01.
+## Task: 01''''
+
+a = int(input("Enter the first number:"))        'Allow to enter the number.'
+b = int(input("Enter the second number:"))        'Allow to enter the number.'
+c=print("The sum of", a, "and", b, "is:", a+b)      ' Take the sum of the two numbers.'
+d=print("The subtraction of", a, "and", b, "is:", a-b)     ' Take the subtraction of the two numbers.'
+e=print("The multiplication of", a, "and", b, "is:", a*b)   ' Take the multiplication of the two numbers.'
+f=print("The division of", a, "and", b, "is:", a/b)           ' Take the division of the two numbers.'
+
+
+'''## Task: 02'''
+
+FstName = input("Enter your first name:")      'Enter the student's first name.'
+LstName = input("Enter your last name:")        'Enter the student's last name.'
+print("Hello,", FstName + " " + LstName+"! Welcome to the python program.")    'Display the output of greetings to the student.'
